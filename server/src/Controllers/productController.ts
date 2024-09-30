b52901ecd6e002cf552d79ba1f7e5aa1073463e5
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-09-30 19:32:06 +0500

    feat: custom http code and errors

--- server/src/Controllers/productController.ts
+++ server/src/Controllers/productController.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { db } from "../db";
 import { catchAsync } from "../utils/errorHandler";
+import createHttpError from "http-errors";
 
 export const getAllProducts = catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
         const products = await db.product.findMany();
 
-        if (products.length === 0) return next(new Error("No Products Found"));
+        if (products.length === 0)
+            return next(createHttpError(404, "No Products Found"));
 
         res.status(200).json({
             status: "success",
